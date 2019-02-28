9801b29f8f44b8a510d57d9887182a2755c4a792
Brian Ly <b.ly1795@gmail.com>
2019-02-27 23:33:05 -0800

    mapQuest.js

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'B7zF9ocrBTnwdRlpZh9oupjfrboTKEyC';
+
+	var map = L.mapquest.map('map', {
+		center: [32.879017, -117.235940],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl: false
+	});
+
+	L.marker([32.879017,-117.235940]).addTo(map);
 }
